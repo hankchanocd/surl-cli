bb5b7e1553e37bec40ed58edb355e40c9790163d
Hank Chan <hankchanth@icloud.com>
2018-04-07 02:04:16 -0700

    Add index file (surl.js)

---
+++ surl.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node --harmony
+
+'use strict';
+
+/**
+ * modules
+ */
+ const program = require('commander');
+ const chalk = require('chalk');
+ const copy = require('copy-to-clipboard');
+
+ const {expandUrl, shortenUrl} = require('./lib/ajax');
+
+// This program is a lightweight cli tool for shortening url using various api providers, 
+// including but not limited to Google, Bitly
+
+program
+.version('1.0.0', '-v, --version')
+.usage('<option> [url]')
+.description(`surl shortens the long url using ${chalk.bold('Google ShortenUrl api')} and ${chalk.bold('Bitly api')}`)
+.option('-r, --reverse', 'Expand url')
+.on('--help', function () {
+    console.log();
+    console.log(`  ${chalk.blueBright('surl [longUrl]')}`);
+    console.log(`  ${chalk.blueBright('=> the shortened url is copied to clipboard [shortUrl]')}`);
+    console.log();
+    console.log(`  ${chalk.blueBright('surl -r [shortUrl]')}`);
+    console.log(`  ${chalk.blueBright('=> the expanded url is copied to clipboard [longUrl]')}`);
+});
+
+
+// logic
+if (!program.options.reverse) {
+    let shortUrl = shortenUrl(program.argv);
+    copy(shortUrl);
+    console.log(`  ${chalk.blueBright('the shortened url is copied to clipboard ' + shortUrl)}`);
+
+} else {
+    let longUrl = expandUrl(program.argv);
+    copy(longUrl);
+    console.log(`  ${chalk.blueBright('the expanded url is copied to clipboard ' + longUrl)}`);
+
+}
+
+
+program.parse(process.argv);
+
+// Display help table when no arg is provided
+if (program.args.length === 0) program.help();
