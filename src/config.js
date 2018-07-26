fbdfd2eb15ce2135f5b379cca1b7428c25e51ef1
Hank Chan <hankchanth@icloud.com>
2018-07-26 12:45:00 -0700

    Reformat style

--- src/config.js
+++ src/config.js
@@ -1,59 +1,63 @@
 /*
- * modules
+ * Configuration
  */
+
+// Dependencies
 const inquirer = require('inquirer');
-const Conf     = require('conf');
-const conf     = new Conf();
+const Conf = require('conf');
+const conf = new Conf();
 
 function inquire() {
-  let questions = [{
-      type: 'list',
-      name: 'provider',
-      message: "Change your URL shortener api provider",
-      choices: ['Google', 'Bitly'],
-      filter: function(val) {
-        return val.toLowerCase();
-      }
-    },
-    {
-      type: 'input',
-      name: 'google_key',
-      message: "Modify your google api key",
-      default: function() {
-        return conf.get('google_key');
-      }
-    },
-    {
-      type: 'input',
-      name: 'bitly_key',
-      message: "Modify your bitly api key",
-      default: function() {
-        return conf.get('bitly_key');
-      }
-    }
-  ];
+    let questions = [{
+            type: 'list',
+            name: 'provider',
+            message: "Change your URL shortener api provider",
+            choices: ['Google', 'Bitly'],
+            filter: function(val) {
+                return val.toLowerCase();
+            }
+        },
+        {
+            type: 'input',
+            name: 'google_key',
+            message: "Modify your google api key",
+            default: function() {
+                return conf.get('google_key');
+            }
+        },
+        {
+            type: 'input',
+            name: 'bitly_key',
+            message: "Modify your bitly api key",
+            default: function() {
+                return conf.get('bitly_key');
+            }
+        }
+    ];
 
-  inquirer.prompt(questions).then(answers => {
-    conf.set('provider', answers.provider);
-    conf.set('google_key', answers.google_key);
-    conf.set('bitly_key', answers.bitly_key);
-    storeShortcutConfiguration();
+    inquirer.prompt(questions).then(answers => {
+        conf.set('provider', answers.provider);
+        conf.set('google_key', answers.google_key);
+        conf.set('bitly_key', answers.bitly_key);
+        storeShortcutConfiguration();
 
-    console.log('saved');
-  });
+        console.log('saved');
+    });
 }
 
 function storeShortcutConfiguration() {
-  if (conf.get('provider') === 'google') {
-    conf.set('providerUrl', 'https://www.googleapis.com/urlshortener/v1/url');
-    conf.set('key', conf.get('google_key'));
-  } else if (conf.get('provider') === 'bitly') {
-    // To be fixed
-    conf.set('providerUrl', 'https://www.googleapis.com/urlshortener/v1/url');
-    conf.set('key', conf.get('bitly_key'));
-  }
+    if (conf.get('provider') === 'google') {
+        conf.set('providerUrl', 'https://www.googleapis.com/urlshortener/v1/url');
+        conf.set('key', conf.get('google_key'));
+    } else if (conf.get('provider') === 'bitly') {
+        // To be fixed
+        conf.set('providerUrl', 'https://www.googleapis.com/urlshortener/v1/url');
+        conf.set('key', conf.get('bitly_key'));
+    }
 }
 
-module.exports = {
-  inquire
+
+// export modules
+export {
+    inquire
 };
