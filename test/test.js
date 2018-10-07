1d61f3e447033a50715b9f1a2c713cf1e9e40296
Hank Chan <hankchanth@icloud.com>
2018-10-07 06:16:35 -0700

    refactor(test.js, npm): Rename the unit test scripts `test:unit`
    BREAKING CHANGE: Change `npm test:no-api` to `npm test:unit`

--- test/test.js
+++ test/test.js
@@ -16,32 +16,22 @@ const apiTest = require('./api-test.js');
 const unitTest = require('./unit-test.js');
 
 
-function test() {
-
+(function test() {
     console.log('\n> Running Tests...');
 
     // Indicate to users that Mocha is modified to allow 6000 ms response time
     console.log(chalk.gray('Warning: Mocha command is modified to allow maximum of 6000 ms response time for API testing\n'));
 
-
     // Determine the running mode
     const currentEnv = typeof (process.env.NODE_ENV) == 'string' ? process.env.NODE_ENV.toLowerCase() : '';
 
-    if (currentEnv === 'no-api') {
-
+    if (currentEnv === 'unit') {
         unitTest();
 
     } else {
-
         apiTest();
-
         unitTest();
     }
 
-};
-
-
-test();
-
-
+})();
 /* jshint ignore:end */
